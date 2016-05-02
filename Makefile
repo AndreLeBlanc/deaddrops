72ae3551522dad54682a6e59cf6d3c66148b0e6b
Jihan <ajoel.wallin@gmail.com>
2016-05-02 16:15:45 +0200

    Basic server and channels

--- Makefile
+++ Makefile
@@ -1,3 +1,2 @@
-# Makefile used to compile the project. 
-
-# If you use another build tool you should remove this file. 
+build:
+	go build
--- server.go
+++ server.go
@@ -3,42 +3,62 @@ package main
 import (
 	"fmt"
 	"net/http"
-//	"html/template"
-//	"strconv"
-	"log"
 	"io"
+	"log"
 	"os"
-//	"crypto/md5"
-//	"time"
 )
 
-func uploadFile(w http.ResponseWriter, r *http.Request){
-    fmt.Println("method:", r.Method)
-    if r.Method == "GET" {
-	    http.ServeFile(w,r, "./filetest/README.md")
-    } else {
-        r.ParseMultipartForm(32 << 20)
-        file, handler, err := r.FormFile("uploadfile")
-        if err != nil {
-            fmt.Println(err)
-            return
-        }
-        defer file.Close()
-        fmt.Fprintf(w, "%v", handler.Header)
-        f, err := os.OpenFile("./filetest/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
-        if err != nil {
-            fmt.Println(err)
-            return
-        }
-        defer f.Close()
-        io.Copy(f, file)
-    }
+func createStash(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Access-Control-Allow-Origin", "*") //TODO: List of allowed server via config file
+
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		//http.ServeFile(w,r, "./filetest/README.md")
+	} else if r.Method == "POST" {
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		fmt.Fprintf(w, "%v", handler.Header)
+		f, err := os.OpenFile("./filetest/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
+	} else {
+		return
+	}
 }
 
-func main(){
-	http.HandleFunc("/upload", uploadFile)
-	err:= http.ListenAndServe(":8080", nil)
+func initServer() {
+	//TODO: check/start database
+
+	//TODO: load server settings from somewhere, ex. port number
+	http.HandleFunc("/", createStash)
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+func initChanMap() map[string]chan string {
+	//TODO: chan string should maybe be a struct instead with relevant data
+	return map[string]chan string{}
+}
+
+func appendChan(m map[string]chan string, token string) {
+	m[token] = make(chan string)
+}
+
+func getChan(m map[string]chan string, token string) chan string {
+	return m[token]
+}
+
+func main() {
+	initServer()
 }
