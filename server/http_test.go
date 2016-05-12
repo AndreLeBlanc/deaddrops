86a9a4ae43eedbea7f6f79332f2adcd68092f2f5
Jihan <ajoel.wallin@gmail.com>
2016-05-12 22:28:56 +0200

    Figured out how to write http POST tests

--- server/http_test.go
+++ server/http_test.go
@@ -3,12 +3,18 @@ package server
 import (
 	"bytes"
 	"deadrop/api"
+	"fmt"
+	"io"
+	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
+	"time"
+	"regexp"
 )
 
-func TestStashGET(t *testing.T) {
+func TestCreate(t *testing.T) {
 	conf := InitServer()
 	cm := conf.upMap
 
@@ -33,28 +39,47 @@ func TestStashGET(t *testing.T) {
 }
 
 // Test does not work yet, as POST is not 100% defined yet
-func TestStashPOST(t *testing.T) {
+func TestUpload(t *testing.T) {
 	conf := InitServer()
 
 	csHandler := makeHandler(create, conf)
-
-	req, _ := http.NewRequest("GET", "http://localhost:9090/create", nil)
+	req, err := http.NewRequest("GET", "http://localhost:9090/create", nil)
+	if err != nil {
+		t.Errorf("Error creating GET /create request")
+	}
 	w := httptest.NewRecorder()
 	csHandler.ServeHTTP(w, req)
 
+	if w.Code != http.StatusOK {
+		t.Errorf("[Create] GET response: %v", w.Code)
+	}
+	body := w.Body.String()
+
+	// Removing '"' characters from string
+	reg := regexp.MustCompile(`"([^"]*)"`)
+	res := reg.ReplaceAllString(body, "${1}")
+	token := string(res)
+	fmt.Println(token)
+	
 	csHandler = makeHandler(upload, conf)
-	var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
-	req, _ = http.NewRequest("POST", "http://localhost:9090/upload", bytes.NewBuffer(jsonStr))
+	req, err = postFile("test1.txt", "http://localhost:9090/upload", token)
+	if err != nil {
+		t.Errorf("Error creating POST /upload request")
+	}
 
 	w = httptest.NewRecorder()
+	// Wait for parent /create request to complete
+	time.Sleep(50 * time.Millisecond)
 	csHandler.ServeHTTP(w, req)
 
-	if w.Code != http.StatusOK {
-		t.Errorf("No Stash POST response: %v", http.StatusOK)
-	}
+	resp_body := w.Body.String()
+	fmt.Println(resp_body)
+
+	// TODO: Do some confirmation with resp_body
 }
 
-func TestEndUpload(t *testing.T) {
+
+func TestFinalize(t *testing.T) {
 	conf := InitServer()
 
 	csHandler := makeHandler(finalize, conf)
@@ -69,4 +94,52 @@ func TestEndUpload(t *testing.T) {
 	if w.Code != http.StatusOK {
 		t.Errorf("No Stash POST response: %v", http.StatusOK)
 	}
+}
+
+func TestStashDownload(t *testing.T) {
+
+}
+
+func TestFileDownload(t *testing.T) {
+
+}
+
+func postFile(filename string, targetUrl string, token string) (*http.Request, error) {
+	bodyBuf := &bytes.Buffer{}
+	bodyWriter := multipart.NewWriter(bodyBuf)
+
+	err := bodyWriter.WriteField("token", token)
+	if err != nil {
+		fmt.Println("error creating field")
+		return nil, err
+	}
+	
+	fileWriter, err := bodyWriter.CreateFormFile("uploadfile", filename)
+	if err != nil {
+		fmt.Println("error writing to buffer")
+		return nil, err
+	}
+	
+	f, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("error opening file")
+		return nil, err
+	}
+
+	_, err = io.Copy(fileWriter, f)
+	if err != nil {
+		
+		return nil, err
+	}
+	
+	contentType := bodyWriter.FormDataContentType()
+	bodyWriter.Close()
+
+	req, err := http.NewRequest("POST", targetUrl, bodyBuf)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", contentType)
+
+	return req, nil
 }
