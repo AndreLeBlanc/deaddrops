17ac495f8a1e22784bce6a8b025119b1a5798a66
André Le Blanc <leblanc_112@hotmail.com>
2016-05-18 10:26:28 +0200

    Beta version klar!

--- database/InsertStash.go
+++ database/InsertStash.go
@@ -1,59 +1,42 @@
 package database
 
 import (
-    "database/sql"
-    "fmt"
-    _ "github.com/mattn/go-sqlite3"
-    "time"
-    "deadrop/api"
+	"database/sql"
+	"deadrop/api"
+	_ "github.com/mattn/go-sqlite3"
+	"time"
 )
 
-func addToHashD(db * sql.DB, tok string, StashNom string, lifeTime int) error {
-    _, err := db.Exec("INSERT INTO stashes(token, StashName, Lifetime) values(?,?,?)", tok, StashNom, lifeTime)
-    return err
+func addToHashD(db *sql.DB, tok string, StashNom string, lifeTime int) error {
+	_, err := db.Exec("INSERT INTO stashes(token, StashName, Lifetime) values(?,?,?)", tok, StashNom, lifeTime)
+	return err
 }
 
-func createNewTable(db * sql.DB, token string, fil * []StashFile) error {
-    if fil == nil {
-        return DError{time.Now(), "No stashfile!"}
-    }   
+func createNewTable(db *sql.DB, token string, fil *[]StashFile) error {
+	if fil == nil {
+		return DError{time.Now(), "No stashfile!"}
+	}
 
-    _ , err := db.Exec("CREATE TABLE IF NOT EXISTS " + token + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, Fname STRING, size INT, type String, numD Int, sqltime TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL)")
-    if err != nil {
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS " + token + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, Fname STRING, size INT, type String, numD Int, sqltime TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL)")
+	if err != nil {
 
-        return DError{time.Now(), "Couldn't create table!"}
-    }
+		return DError{time.Now(), "Couldn't create table!"}
+	}
 
-    for _ ,element := range *fil {
-        _, error := db.Exec("INSERT INTO " + token + " (Fname, Size, Type, numD) values(?,?,?,?)", element.Fname, element.Size, element.Type, element.Download)      
-        if error != nil {
-            return DError{time.Now(), "Couldn't insert into table!"}
-        }
-    }
+	for _, element := range *fil {
+		_, error := db.Exec("INSERT INTO "+token+" (Fname, Size, Type, numD) values(?,?,?,?)", element.Fname, element.Size, element.Type, element.Download)
+		if error != nil {
+			return DError{time.Now(), "Couldn't insert into table!"}
+		}
+	}
 
-    return nil
+	return nil
 }
 
-
-func skrivUt(db * sql.DB) {
- 	rows, err := db.Query("SELECT * FROM userinfo")
-    CheckErr(err)
-
-        for rows.Next() {
-	        var hash, size, numD int
-	        var format string
-            var time time.Time
-	        err = rows.Scan(&hash, &size, &format, &numD, &time)
-	        CheckErr(err)
-	        fmt.Println(hash , " | ", size, " | ", time, " | ", format, " | ", numD)
-    }
-}
-
-
-func InsertStash(db * sql.DB, s * Stash) error {
-    error := addToHashD(db, s.Token, s.StashName, s.Lifetime)
-    if error == nil {
-        error = createNewTable(db, s.Token, &s.Files)
-    }
-    return error
+func InsertStash(db *sql.DB, s *Stash) error {
+	error := addToHashD(db, s.Token, s.StashName, s.Lifetime)
+	if error == nil {
+		error = createNewTable(db, s.Token, &s.Files)
+	}
+	return error
 }
--- database/dbMod.go
+++ database/dbMod.go
@@ -4,22 +4,22 @@ import (
 	"database/sql"
 )
 
-func Init() * sql.DB {
+func Init() *sql.DB {
 	Db, err := sql.Open("sqlite3", "database/deadrops.db")
-    CheckErr(err)
-    return Db
+	CheckErr(err)
+	return Db
 }
 
 func CheckErr(err error) {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
 
-func Close(db * sql.DB) {
+func Close(db *sql.DB) {
 	db.Close()
 }
 
 func (e DError) Error() string {
-    return fmt.Sprintf("%v: %v", e.When, e.What)
+	return fmt.Sprintf("%v: %v", e.When, e.What)
 }
